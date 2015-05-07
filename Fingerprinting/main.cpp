20895542a1918da6b06fb2c64cb44e1aede90a97
Daniel Balcells <dbalcells@gmail.com>
2015-05-07 11:29:16 +0200

    Proves

---
+++ Fingerprinting/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: daniel
+ *
+ * Created on 7 de mayo de 2015, 11:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
