4970602a235edd9d25c5c6dcf952b1c67598e3e4
Daniel Balcells <dbalcells@gmail.com>
2015-05-09 15:58:12 +0200

    Crear arxius per computeSpectrogram, computeLog, subtractMean i hpFilterRows.

---
+++ computeLog.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   computeLog.cpp
+ * Author: daniel
+ *
+ * Created on 9 de mayo de 2015, 15:45
+ */
+
+#include <cstdlib>
+#include "aquila/transform/Spectrogram.h"
+
+using namespace std;
+
+/*
+ * Computes the logarithm of the input spectrogram.
+ */
+Aquila::Spectrogram computeLog(Aquila::Spectrogram spectrogram){
+
+return 0;
+}
