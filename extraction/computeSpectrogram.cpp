d7ae0865c187479a3e2bfc4d19d5859672f143b0
Enric Calabuig <enric.calabuig@gmail.com>
2015-05-09 17:22:52 +0200

    arreglao

--- extraction/computeSpectrogram.cpp
+++ extraction/computeSpectrogram.cpp
@@ -1,19 +1,16 @@
 #include <cstdlib>
 #include "aquila/source/WaveFile.h"
 #include "aquila/transform/Spectrogram.h"
-//#include "RecomovieSpectrogram.h"
+#include "RecomovieSpectrogram.h"
 
 /*
-<<<<<<< HEAD
  * This function takes a WaveFile as input and computes its spectrogram.
  */
-Aquila::Spectrogram computeSpectrogram(Aquila::WaveFile inFile) {
-    
-=======
+
+/*    
  * Computes the spectrogram of an audio file.
  */
 RecomovieSpectrogram computeSpectrogram(Aquila::WaveFile inFile) {
->>>>>>> 58fc18f9f825978ee2870ed922512ed82fd33e6f
     Aquila::FramesCollection frames;
     frames = Aquila::FramesCollection(inFile, 512, 256);
     RecomovieSpectrogram spectrogram = RecomovieSpectrogram(frames);
