d44cba87aa51a31dea0f952af0f8913b1edadd34
Daniel Balcells <dbalcells@gmail.com>
2015-05-09 16:29:29 +0200

    Canviar Aquila::Spectrogram per Aquila::RecomovieSpectrogram

---
+++ extractMaxes.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   extractMaxes.cpp
+ * Author: daniel
+ *
+ * Created on 9 de mayo de 2015, 16:21
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int** extractMaxes(Aquila::RecomovieSpectrogram hpfZMLSpectrogram){
+
+    return 0;
+}
